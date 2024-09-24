ac20f92c8ba15720758b5cfde2117d421b097372
Alex Bohnson <Bohnson@gmail.com>
2024-09-23 23:18:48 -0700

    Expanded API and router

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js';
-import product from './models/product.model.js';
-import Product from './models/product.model.js';
+import productRoutes from './routes/product.route.js';
 
 dotenv.config();
 
@@ -10,33 +9,10 @@ const  app = express();
 
 app.use(express.json()); //middleware that allows us to accept json data in the req.body
 
-app.get("/", (req, res) => {{
-    res.send("API is running");
-}});
-
-app.post("/api/products", async (req, res) => {
-    const product = req.body; //user will send this data
-
-    if (!product.name || !product.price || !product.imageUrl) {
-        return res.status(400).json({success:false, message: "Please provide all fields"});
-    }
-
-    const newProduct = new Product(product); //product from model.js
-
-    try {
-        await newProduct.save();
-        res.status(201).json({success:true, data: newProduct}); //201 means something created
-    } catch (error) {
-        console.error("error in create product", error.message);
-        res.status(500).json({success:false, message: "Server error"});
-    }
-
-    //postman
-
-});
+app.use("/api/products", productRoutes);
 
 app.listen(3000, () => {
     connectDB();
-    console.log("Server is running on port 3000")
-})
+    console.log("Server is running on port 3000");
+});
 
