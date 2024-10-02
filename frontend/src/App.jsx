92cd1f80de50456733869b0a036387d5b366c401
Alex Bohnson <Bohnson@gmail.com>
2024-10-01 22:14:58 -0700

    Frontend progress. Added jsx pages and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
-import { Button } from "@chakra-ui/react"
+import { Box} from "@chakra-ui/react"
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
+import NavBar from "./components/Navbar";
+
 
 function App() {
   return (
     <>
-      <Button>Hello</Button>
+      <Box minH={"100vh"}>
+      <NavBar /> {/*keeps Navbar at the top of every page */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+
+        </Routes>
+
+      </Box>
     </>
   );
 }
