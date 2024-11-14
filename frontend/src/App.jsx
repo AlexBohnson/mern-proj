9d474131f63b3a99dfe46a531622d44166defee4
Alex Bohnson <Bohnson@gmail.com>
2024-11-13 23:49:35 -0800

    Switch to vanilla ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,11 @@
 
-import { Button } from "./components/ui/button"
 import React from "react"
-import {
-  MenuContent,
-  MenuItem,
-  MenuRoot,
-  MenuTrigger,
-} from "./components/ui/menu"
-import { Stack } from "@chakra-ui/react"
+
 
 function App() {
 
   return (
-    <Stack>
-      <Button size='sm'>
-        Test Button 1 Baby
-      </Button>
-
-      <Button size='sm'>
-        Test Button 1 Baby
-      </Button>
-
-      <Button size='sm'>
-        Test Button 1 Baby
-      </Button>
-
-      <MenuRoot>
-        <MenuTrigger />
-        <MenuContent>
-          <MenuItem value="..." />
-        </MenuContent>
-      </MenuRoot>
-
-    </Stack>
-
-
+    <></>
   )
 }
 
